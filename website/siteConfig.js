da8ae1d637d1ba916f71e6d6cc3e96c75cdbf851
Topher Hindman <christopher.hindman@dbtlabs.com>
2023-04-11 07:01:47 -0700

    update project name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'dbt Labs Handbook',
+  projectName: 'handbook',
   organizationName: 'dbt Labs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
